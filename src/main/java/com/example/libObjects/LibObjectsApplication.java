389d29be6bea056fada906c21a573dc7a87d06a4
Breno Lima <85833737+BrenoLim4@users.noreply.github.com>
2023-12-19 14:27:51 -0300

    primeiro commit

---
+++ src/main/java/com/example/libObjects/LibObjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.libObjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibObjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibObjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libObjects/LibObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libObjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibObjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
